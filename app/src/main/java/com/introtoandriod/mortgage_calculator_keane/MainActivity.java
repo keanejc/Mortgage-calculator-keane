5d29311aedf010ec9f5c0fa95c2227457ea46cbb
keanejc <keanejc@g.cofc.edu>
2017-02-07 15:51:27 -0500

    Initial commit

---
+++ app/src/main/java/com/introtoandriod/mortgage_calculator_keane/MainActivity.java
@@ -0,0 +1,13 @@
+package com.introtoandriod.mortgage_calculator_keane;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
