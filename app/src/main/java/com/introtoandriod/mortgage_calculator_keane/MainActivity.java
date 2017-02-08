c9327120c292c59790e77d3becf9a95860df8b9d
keanejc <keanejc@g.cofc.edu>
2017-02-08 14:14:08 -0500

    set up intents and buttons

--- app/src/main/java/com/introtoandriod/mortgage_calculator_keane/MainActivity.java
+++ app/src/main/java/com/introtoandriod/mortgage_calculator_keane/MainActivity.java
@@ -1,13 +1,40 @@
 package com.introtoandriod.mortgage_calculator_keane;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button calculate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        calculate = (Button) findViewById(R.id.button);
+
+        calculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, mortgageSummary.class));
+            }
+
+        });
+
+
     }
+
+    @Override
+    public void onSaveInstanceState( Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState);
+
+    }
+
+    @Override
+    public void onRestoreInstanceState( Bundle savedInstanceState){
+        super.onRestoreInstanceState(savedInstanceState);
+    }
+
 }
--- app/src/main/java/com/introtoandriod/mortgage_calculator_keane/mortgageSummary.java
+++ app/src/main/java/com/introtoandriod/mortgage_calculator_keane/mortgageSummary.java
@@ -1,29 +1,32 @@
 package com.introtoandriod.mortgage_calculator_keane;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class mortgageSummary extends AppCompatActivity {
+    private Button home;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mortgage_summary);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+       home = (Button) findViewById(R.id.button2);
+
+        home.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                startActivity(new Intent(mortgageSummary.this, MainActivity.class));
             }
+
         });
+
     }
 
 }
